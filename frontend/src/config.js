27dcb88b3e28fcfede8e303c20929f4359d68869
Léo <leopluche@hotmail.fr>
2025-07-23 19:24:16 +0200

    new run build with /

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const API_BASE = import.meta.env.VITE_API_URL
 
 export const ENDPOINTS = {
-  sudoku : (level) => `${API_BASE}api/sudoku?level=${level}`,
-  leaderboard : (level) => `${API_BASE}api/leaderboard?level=${level}`,
-  postScore : `${API_BASE}api/leaderboard`
+  sudoku : (level) => `${API_BASE}/api/sudoku?level=${level}`,
+  leaderboard : (level) => `${API_BASE}/api/leaderboard?level=${level}`,
+  postScore : `${API_BASE}/api/leaderboard`
 }
